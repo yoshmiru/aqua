a217aa5e7c9951659caf876027134e9eff1b2289
Yoshiaki Miura <yos1127@gmail.com>
2017-10-10 18:21:08 +0900

    add feed-1.py

---
+++ feed-1.py
@@ -0,0 +1,22 @@
+from serial import Serial
+from time import sleep
+
+ser = Serial('/dev/ttyACM0')
+sleep(2)
+ser.write(b'FEED:180')
+print(ser.readline().decode('utf-8'))
+sleep(0.5)
+ser.write(b'FEED:175')
+print(ser.readline().decode('utf-8'))
+sleep(0.5)
+ser.write(b'FEED:180')
+print(ser.readline().decode('utf-8'))
+sleep(0.5)
+ser.write(b'FEED:175')
+print(ser.readline().decode('utf-8'))
+sleep(0.5)
+ser.write(b'FEED:180')
+print(ser.readline().decode('utf-8'))
+sleep(0.5)
+ser.write(b'FEED:100')
+print(ser.readline().decode('utf-8'))
