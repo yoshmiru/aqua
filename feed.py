8a20d34ad4b53d737d6739c47835bbfcdf71a86b
Yoshiaki Miura <yos1127@gmail.com>
2017-11-03 14:18:43 +0900

    update feed

--- feed.py
+++ feed.py
@@ -1,22 +1,17 @@
+#!/usr/bin/env python
 from serial import Serial
 from time import sleep
 
-ser = Serial('/dev/ttyACM0')
-sleep(2)
-ser.write(b'S')
-ser.write(b'180')
-sleep(0.5)
-ser.write(b'S')
-ser.write(b'175')
-sleep(0.5)
-ser.write(b'S')
-ser.write(b'180')
-sleep(0.5)
-ser.write(b'S')
-ser.write(b'175')
-sleep(0.5)
-ser.write(b'S')
-ser.write(b'180')
-sleep(2)
-ser.write(b'S')
-ser.write(b'100')
+def feed(ser):
+    ser.write(b'FEED:180')
+    print(ser.readline())
+    ser.write(b'FEED:100')
+    print(ser.readline())
+
+
+if __name__ == '__main__':
+    ser = Serial('/dev/ttyACM0')
+    sleep(2)
+    feed(ser)
+    #feed(ser)
+    #feed(ser)
